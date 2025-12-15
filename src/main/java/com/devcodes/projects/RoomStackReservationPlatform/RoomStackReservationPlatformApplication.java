47f19ce6cd62d8f882dca546794da4ab44134115
devansh67 <devansh67@gmail.com>
2025-12-16 03:54:06 +0530

    Project setup

---
+++ src/main/java/com/devcodes/projects/RoomStackReservationPlatform/RoomStackReservationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.devcodes.projects.RoomStackReservationPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomStackReservationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomStackReservationPlatformApplication.class, args);
+	}
+
+}
